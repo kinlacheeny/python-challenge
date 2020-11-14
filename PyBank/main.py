a79c4cbf7e59ad44cfc8bf0de36941e92d23f426
GHWIC <GHWIC@Goods-MacBook-Pro.local>
2020-11-14 17:25:56 -0500

    getting somewhere

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,17 +1,79 @@
 import os
 import csv
-csvpath = os.path.join( 'Resources', 'budget_data.csv')
 
-with open(csvpath) as csv_file:
+#Define the variables
+months = []
+profit_loss_change = []
+
+total_mos = 0
+net_profit_loss = 0
+prev_mos_profit_loss = 0
+curr_mos_profit_loss = 0
+profit_loss_change = 0
+
+# Define path
+main_csv = os.path.join( 'Resources', 'budget_data.csv')
+
+# Open and read csv
+with open(main_csv) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
 
-    for row in csv_reader:
-        print (row)
+    # Read header row 
+    csv_header = next(csv_file)
+
+    print(f"Header: {csv_header}")
+
+
+ 
+    #for row in csv_reader:
+     #   print (row[0])
+
+
+
+'''total_months = 0
+total_profit_losses = 0
+
+for row in csv_reader:
+  
+  
+  date = row[0]
+  profit_losses = row[1]
+  total_months = 
+  total_profit_losses = int(row[1])
+
+  print(total_months)
+'''
+# Create a function and have it accept 'final_data' as its sole parameter
+#def final_data(main_data):
+
+    # Assign values to variables with descriptive names
+ #   months = str(main_data[0])
+ #   profit_losses = float(main_data[1])
+
+    # * The total number of months included in the dataset - 
+ #   final_data(months).sum()
 
 '''
+    total_months = 0  
+    total_profit_losses = 0
+
+for row in csv_reader: #(***3.3 1:52:00)
+    date = row[0]
+    profit_losses = row[1]
+    total_months += 1
+    total_profit_losses += int(row[1])
+
+bank_data = []
+
+for row in csv_reader:
+    profit_losses = row[1]
+    bank_data.append(profit_losses)
+
+
+
 * Your task is to create a Python script that analyzes the records to calculate each of the following:
   
-  * The total number of months included in the dataset
+  * The total number of months included in the dataset - 
 
   * The net total amount of "Profit/Losses" over the entire period
 
